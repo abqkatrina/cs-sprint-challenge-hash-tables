02c6fbcdd4462a17217524f587d769436f963fad
Katrina Hernandez <abqkatrina@gmail.com>
2020-08-07 09:36:46 -0600

    ex2 passed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,33 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    Your function should output an array of strings with the entire route of
+    your trip. For the above example, it should look like this:
+
+    ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+
+    Your solution should run in linear time. You can assume that your
+    function will always be handed a valid ticket chain as input. 
+
+    * We can hash each ticket such that the starting location is the key and
+    the destination is the value. Then, when constructing the entire
+    route, the `i`th location in the route can be found by checking the
+    hash table for the `i-1`th location.
+    
     """
-    # Your code here
+    route = []
+    cache = {}
+    
+    # add tickets to cache: k is source, v is dest
+    for t in tickets:
+        if t not in cache:
+            cache[t.source] = t.destination
+    
+    # need to establish an index range 
+    route.append(cache['NONE'])
+
+    # return each destination
+    for i in range(length-1):
+        route.append(cache[route[i]])
 
     return route
